c597e9ec0683470b968fa49e72aaecf2471e0ba3
Joseph Achacha <odekachacha@gmailcom>
2018-11-23 18:19:03 +0300

    Modified Index.js by adding a function to perfom multiplication

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+//Addition function
 function addition (a, b){
    var result = a + b;
   console.log(`Total: ${result}`);
 }
+// Multiplication function
+function multi(a, b){
+   var final = a * b;
+  console.log(`Total: ${final}`);
+}
 
 addition(4, 8);
+multi(4, 4);
